3a3dc152caee15073896d0bee2670c02b4d05190
Jacob Adamson <jacob.e.adamson@gmail.com>
2017-08-17 22:38:28 -0700

    Fixed lesbian lovin' resulting in pregnancy, pregnancy discovery, and child-birth with dead father

--- Source/scripts/copy-assets.sh
+++ Source/scripts/copy-assets.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf "Assemblies/"*
-cp "Source/RimWorld-Children/obj/Debug/RimWorldChildren.dll" "Assemblies/"
+cp "Source/RimWorld-Children/obj/Release/RimWorldChildren.dll" "Assemblies/"
